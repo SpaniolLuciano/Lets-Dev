5d6e9c588019908b55d535b96a309c714f9e28c2
SpaniolLuciano <luciano4123spaniol@gmail.com>
2022-07-10 17:19:31 -0300

    Correção footer

--- LetsDev/src/componentes/index.ts
+++ LetsDev/src/componentes/index.ts
@@ -1,6 +1,6 @@
 import ColumnInput from "./column-input/column-input";
 import InputButton from "./input-button/input-button";
-import Footer from "../footer/footer";
+import Footer from "./footer/footer";
 import Label from "./label/label";
 import Row from "./row/row";
 import RowSelectors from "./row-selectors/row-selectors";
